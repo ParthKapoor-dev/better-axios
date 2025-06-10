61bcf489964d67f430d6c34ea003146be4dde16c
Parth Kapoor <parthkapoor.coder@gmail.com>
2025-06-11 01:24:41 +0530

    web/ landing page --init

--- web/app/layout.config.tsx
+++ web/app/layout.config.tsx
@@ -11,14 +11,12 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
+        <img
+          src={"/better-axios-logo.png"}
+          width={24}
+          height={24}
+          className="rounded-full"
+        />
         Better Axios
       </>
     ),
